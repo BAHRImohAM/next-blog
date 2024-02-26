9b528052ed4785821d867c77698eb354c9b889cf
BAHRImohAM <ma.bahri@esi-sba.dz>
2024-02-26 22:26:14 +0100

    db connection testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
-export default function Home() {
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export default async function Home() {
+  const data = await prisma.test.create({
+    data: {
+      test: "Hi"
+    }
+  });
   return (
-    <div>Hi</div>
+    <div>{JSON.stringify(data)}</div>
   );
 }
